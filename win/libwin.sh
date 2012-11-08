0afe5165d1c9a44ca12a75def46c97adaf3bf5db
w_z_y <allspace@gmail.com>
2012-11-08 00:10:29 +0000

    win support

--- win/libwin.sh
+++ win/libwin.sh
@@ -1,38 +1,70 @@
+export HISTIGNORE="*"
+CMD_SMB="pwdfiller smbclient"
+CMD_NET="pwdfiller net rpc service"
 
 TARGET_VOLUME="c"
-AGENT_FILENAME="fexec_agent.exe"
+TARGET_FILENAME="fexec_agent.exe"
+TARGET_PATH="$TARGET_VOLUME:\\fexec\\$TARGET_FILENAME"
+TARGET_SERVICE="//${SERVER_HOST}/${TARGET_VOLUME}\$";
 
-function push_agent()
-{
-	local SERVICE="//${SERVER_HOST}/${TARGET_VOLUME}\$";
-	local CMD="mkdir fexec; setmode fexec +hs; put ${AGENT_FILENAME} \\fexec\\${AGENT_FILENAME}"
-	`smbclient ${SERVICE} -c ${CMD} -U ${USERNAME}%${PASSWORD}`
-
-}
-
-function clean_agent()
-{
-	local SERVICE="//${SERVER_HOST}/${TARGET_VOLUME}\$";
-	local CMD="rm \\fexec\\*; rmdir \\fexec"
-	`smbclient ${SERVICE} -c ${CMD} -U ${USERNAME}%${PASSWORD}`
-}
 
 function start_agent()
 {
 	local RC="";
+	local SERVER_HOST=$1;
 	
-	`net rpc service create fexec fexec "c:\\fexec\\agent.exe" -I ${SERVER_HOST} -U ${USERNAME}%${PASSWORD}`
-	RC=$?
+	`$CMD_NET fexec fexec "$TARGET_PATH" -I ${SERVER_HOST} -U ${USERNAME} 2>/dev/null 0<<<"${PASSWORD}"`
+	[ "$?" != "0" ] && return 1;
 	
-	`net rpc service start fexec -I ${SERVER_HOST} -U ${USERNAME}%${PASSWORD}`
-	RC=$?
+	`$CMD_NET start fexec -I ${SERVER_HOST} -U ${USERNAME} 2>/dev/null 0<<<"${PASSWORD}"`
+	[ "$?" != "0" ] && return 2;
 	
-	return $RC;
+	return 0;
 }
 
 function stop_agent()
 {
-	`net rpc service stop fexec -I ${SERVER_HOST} -U ${USERNAME}%${PASSWORD}`
-	`net rpc service delete fexec -I ${SERVER_HOST} -U ${USERNAME}%${PASSWORD}`
+	local SERVER_HOST=$1;
+	
+	`$CMD_NET stop fexec -I ${SERVER_HOST} -U ${USERNAME} 2>/dev/null 0<<<"${PASSWORD}"`
+	[ "$?" != "0" ] && return 1;
+	
+	`$CMD_NET delete fexec -I ${SERVER_HOST} -U ${USERNAME} 2>/dev/null 0<<<"${PASSWORD}"`
+	[ "$?" != "0" ] && return 2;
+	
+	return 0;
 }
 
+function install_agent()
+{
+	local RC=""
+	local SERVER_HOST=$1;
+	local CMD="mkdir fexec; setmode fexec +hs; put ${TARGET_FILENAME} \\fexec\\${TARGET_FILENAME}; ls \\fexec\\${TARGET_FILENAME};"
+	RC=`$CMD_SMB ${TARGET_SERVICE} -U ${USERNAME} -c "${CMD}" 2>/dev/null 0<<<"$PASSWORD" | grep NT_STATUS_NO_SUCH_FILE`
+	[ -n "$RC" ] && return 1;
+	
+	start_agent "$SERVER_HOST";
+	[ "$?" != "0" ] && return 2;
+	
+	return 0;
+}
+
+function uninstall_agent()
+{
+	local SERVER_HOST=$1;
+	
+	stop_agent "$SERVER_HOST";
+	[ "$?" != "0" ] && return 1;
+	
+	local CMD="rm \\fexec\\*; rmdir \\fexec"
+	`$CMD_SMB ${TARGET_SERVICE} -U ${USERNAME} -c "${CMD}" 2>/dev/null 0<<<"${PASSWORD}"`
+	return $?;
+}
+
+function exec_script()
+{
+	local SCRIPT=$1;
+	local CMD_PUT="put _sin \\fexec\_fin; rename \\fexec\_fin \\fexec\fin;";
+	local CMD_GET="get \\fexec\fout";
+
+}
