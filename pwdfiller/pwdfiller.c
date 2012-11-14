6e2bf9b669e9b3c2c512b956b67fe58998c4ca28
w_z_y <allspace@gmail.com>
2012-11-14 14:58:58 +0000

    pwdfiller enhance

--- pwdfiller/pwdfiller.c
+++ pwdfiller/pwdfiller.c
@@ -6,7 +6,7 @@
 
 int print_help()
 {
-	printf("Usage: pwdfiller <shell command>");
+	printf("Usage: pwdfiller -c <shell command>\n");
 
 	return 0;
 }
@@ -17,8 +17,8 @@ int pexec(char *cmd, char *pwd)
 	int rc = 0;
 	FILE *fin = NULL;
 
-        setpgid(0, getppid());
-	setsid();
+    setpgid(0, getppid());
+	setsid();					//drop the controlling tty
 
 	fin = popen(cmd, "w");
 	if(fin==NULL) 
@@ -28,8 +28,9 @@ int pexec(char *cmd, char *pwd)
 	}
 
 	fputs(pwd, fin);
-	pclose(fin);
-
+	rc = pclose(fin);
+	rc = rc<0 ? rc : WEXITSTATUS(rc);
+	
 shutdown:
 	return rc;
 }
@@ -40,25 +41,33 @@ int main(int argc, char * argv[])
 {
 	int rc = 0;
 	char pwd[128];
+	char cmd[10240]="exec";
 
-	if(argc<2)
+	if(argc<3)
 	{
 		print_help();
 		exit(1);
-	}	
+	}
+	
+	int i = 0;
+	for(i=2; i<argc; i++)
+	{
+		sprintf(cmd, "%s '%s'", cmd, argv[i]);	//there cannot be "'" in the arguments
+	}
+	//printf("%s\n", cmd);
 	
 	fgets(pwd, sizeof(pwd), stdin);
 	int len = strlen(pwd);
 	if(pwd[len-1]=='\n') pwd[len-1] = '\0';
 
-	rc = pexec(argv[1], pwd);	
-	if(rc != 0)
+	rc = pexec(cmd, pwd);	
+	if(rc == -1)
 	{
 		printf("E: failed to execute the command.");
-		exit(1);
+		exit(255);
 	}
 
-	return 0;
+	return rc;
 }
 
 
